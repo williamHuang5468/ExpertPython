96adf8295d1ce2eef05dbcbb51c6802c19cb008d
williamhuang <chobits5468@gmail.com>
2015-02-15 17:31:27 +0800

    [Init] First Version of SubClass

---
+++ src/Classes/SubClass/SubClass.py
@@ -0,0 +1,10 @@
+class defaultDict(dict):
+    def __init__(self, default=None):
+        dict.__init__(self)
+        self.default = default
+
+    def __getitem__(self, key):
+        try:
+            return dict.__getitem____(self, key)
+        except KeyError:
+            return self.default
